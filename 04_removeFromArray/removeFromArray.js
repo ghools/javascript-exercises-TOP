b4f765ae29416e773707f43f7eb6b0b06a06feb5
ghool <santad06@yahoo.com>
2022-06-15 16:44:53 -0400

    Finish assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayOG, ...removed ) {;
+    return newArray = arrayOG.filter(remover => !removed.includes(remover));
 };
 
 // Do not edit below this line
