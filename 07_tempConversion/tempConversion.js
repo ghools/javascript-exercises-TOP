03a1e6aeb4946dab4d505b0c82909f910bee0036
ghool <santad06@yahoo.com>
2022-06-16 16:21:44 -0400

    Finish assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fehIn) {
+  let celOut = (fehIn - 32) * (5/9)
+  return Math.round(celOut * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celIn) {
+  let fehOut = celIn * (9/5) + 32;
+  return Math.round(fehOut * 10) / 10;
 };
 
 // Do not edit below this line
