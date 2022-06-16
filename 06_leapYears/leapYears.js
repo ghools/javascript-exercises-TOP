ca0d3361b132768b7a4aefda76d18ca8585a3e3a
ghool <santad06@yahoo.com>
2022-06-16 16:11:57 -0400

    Finish assignment 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear;
 
+    if(year % 400 === 0) {
+        leapYear = true;
+        return leapYear;
+    }
+    if(year % 100 === 0) {
+        leapYear = false;
+        return leapYear;
+    }
+    if(year % 4 === 0) {
+        leapYear = true;
+        return leapYear;
+    }
+    else {
+        leapYear = false;
+        return leapYear;
+    }
 };
 
 // Do not edit below this line
