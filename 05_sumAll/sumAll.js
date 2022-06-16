c179f159891366317dbbce50396e51f5ac65f802
ghool <santad06@yahoo.com>
2022-06-16 15:21:50 -0400

    Finish assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(!Number.isFinite(num1) || !Number.isFinite(num2)|| num1 < 0 || num2 < 0) {
+        sum = "ERROR"
+        return sum;
+    }
+    else {
+        let min = Math.min(num1, num2)
+        let max = Math.max(num1, num2)
+    
+        for (let i = min; i <= max; i++){
+            sum += i;
+        }
+        return sum;
+    }
 
 };
 
