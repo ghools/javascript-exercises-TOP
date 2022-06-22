8984378424bb850ae2cf50ea94835eca5958de97
ghool <santad06@yahoo.com>
2022-06-22 16:27:55 -0400

    Finish assignment 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
+const add = function(a, b) {
+  let sum = a +b;
+  return sum;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let sum = a -b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum =0;
+  for(let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  if(a[0] === NaN){
+    return -1;
+  }
+  let sum = 1;
+  for(let i = 0; i <a.length; i++) {
+    sum *= a[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a**b;
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  if(a < 0) {
+    return -1;
+  }
+  while(a > 1){
+    sum *= a;
+    a--
+  }
+	return sum;
 };
 
 // Do not edit below this line
