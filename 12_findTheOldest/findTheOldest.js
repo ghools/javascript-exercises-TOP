93f8f609813ae1b37f4f6c16276806eaf3e32284
ghool <santad06@yahoo.com>
2022-06-23 15:52:54 -0400

    Finish assignment 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    function addAge(people){
+        if(people.yearOfDeath === undefined || null){
+          people.yearOfDeath = 2022;
+        }
+        people.age = people.yearOfDeath - people.yearOfBirth;
+      }
+      
+      people.forEach(addAge);
+      people.sort(function(a, b){
+        return b.age - a.age; 
+      })
+      return people[0];
 };
 
 // Do not edit below this line
