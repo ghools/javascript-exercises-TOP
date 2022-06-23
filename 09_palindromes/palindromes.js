d0cafe5646d5d4188c2450226e5494722c3880f6
ghool <santad06@yahoo.com>
2022-06-23 14:01:25 -0400

    Complete assignment 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+//convert string to an array of chars
+//if reading array from start to finish === reading array from end to start, then true. else false
 
+const palindromes = function (string) {
+    let stnString = string.toLowerCase().replace(/[^a-z0-9]/gi, "");
+    let aReverse = stnString.split("").reverse();
+    let rvrString = aReverse.join("");
+    if(stnString === rvrString){
+        return true;
+    }
+    else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
